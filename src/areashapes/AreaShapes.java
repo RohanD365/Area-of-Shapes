30a6ae48b44f035744acbcdee0a48643245ce3c5
rajdo <rajdo@10.0.0.69>
2021-05-17 10:03:13 -0400

    Modular-Inheritance Assignment

---
+++ src/areashapes/AreaShapes.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areashapes;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+
+
+public class AreaShapes {
+
+    double length;
+    double width;
+    double base;
+    double height;
+    double radius;
+    
+    public double getValue(String instruction){
+        // helper method to bullet proof and force user to enter a double value
+        System.out.print(instruction);
+        Scanner input = new Scanner(System.in);
+        double val;
+        while (true){
+            try{
+                val=input.nextDouble();
+                break;
+            }
+            catch (InputMismatchException err){
+                input.next();
+                System.out.print("Not a number, try again! "+instruction);
+            }
+        }
+        return val;
+    }
+    
+    
+    public void userInput(){
+        // allow user to enter the a, b, anc c values of a standard equation
+        Scanner input = new Scanner(System.in);
+        System.out.println("The value of the length and width must not be less than 0!");
+        System.out.println("What is the length of the rectangle");
+        
+        while (true){
+          
+            this.length=getValue("L: ");
+            if (this.length > 0) break;
+            else System.out.println("The length must not be less than 0");
+        }
+        
+        while (true){
+            
+            this.width=getValue("Enter the width: ");
+            if (this.width > 0) break;
+            else System.out.println("The width must not be less than 0"); 
+            
+            
+        }    
+            
+            
+            
+    }
+    
+    public double findArea (){
+       return Math.round (length*width)*100d/100d; 
+        
+    }
+    
+    public void results (Object area){
+        
+        System.out.println("The area of the rectangle is " +this.findArea());
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        AreaShapes a1 = new AreaShapes();
+        a1.userInput();
+        a1.results(a1);
+    }
+    
+}
